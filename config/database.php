b09ad0e927c4fbc8cf9ebda1833ad2684872554b
el4l3x <davidlopez.fya@gmail.com>
2024-03-01 16:50:49 -0400

    database

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'presta' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '185.47.244.83'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'presta17test',
+            'username' => 'user_prestatest',
+            'password' => '&a0aY3m0t',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => 'gfc_',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
